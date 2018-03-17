0517f84e7293c820c24c59864677de44b94dca42
huntersfuntime <hunterorges@gmail.com>
2018-03-16 20:14:20 -0600

    mad lib component created

---
+++ src/components/madlib_form.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class MadlibForm extends Component {
+    render() {
+        return <h1>Mad Libs Form Component {this.props.name}</h1>;
+    }
+}
+
+export default MadlibForm;
