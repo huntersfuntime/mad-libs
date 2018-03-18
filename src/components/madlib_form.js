d5774bed32b2f0f63c808d4ea97c4301d8fdd152
huntersfuntime <hunterorges@gmail.com>
2018-03-18 16:08:03 -0600

    modified handle change function to accept props and set state within the function

--- src/components/madlib_form.js
+++ src/components/madlib_form.js
@@ -6,8 +6,23 @@ import {
 
 class MadlibForm extends Component {
 
-    handleChange = function() {
-        console.log('trying to handle change');
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            color: '',
+            pluralNoun: '',
+            adjectiveOne: '',
+            celebrityOne: ''
+        }
+    }
+
+    handleChange = function(props) {
+        return function(event) {
+        console.log(`value for input ${props.inputTitle} is: ${event.target.value}`)
+        this.setState({[props.inputTitle]: event.target.value});
+        console.log(`value for state ${props.inputTitle} is: ${this.state[props.inputTitle]}`)
+        }.bind(this);
     }
 
     render() { 
@@ -19,9 +34,14 @@ class MadlibForm extends Component {
                         <label className="greenlabel">1</label>
                     </Col>
                     <Col md="10">
-                        <input placeholder="Color" type="text" onChange={this.handleChange}/>
+                        <input placeholder="Color" type="text" onChange={this.handleChange({inputTitle: 'color'})}/>
                     </Col>
                </Row>
+                <Row>
+                   <Col md="12">
+                       <div className="inputDescription">Color</div>
+                   </Col>
+                </Row>
             </Col>
             <Col md="3" className="inputWrapper">
                <Row>
@@ -29,9 +49,14 @@ class MadlibForm extends Component {
                         <label className="greenlabel">1</label>
                     </Col>
                     <Col md="10">
-                        <input placeholder="Color" type="text" onChange={this.handleChange} />
+                        <input placeholder="Noun" type="text" onChange={this.handleChange({inputTitle: 'pluralNoun'})} />
                     </Col>
                </Row>
+                <Row>
+                   <Col md="12">
+                       <div className="inputDescription">Noun</div>
+                   </Col>
+                </Row>
             </Col>
             <Col md="3" className="inputWrapper">
                <Row>
@@ -39,9 +64,14 @@ class MadlibForm extends Component {
                         <label className="greenlabel">1</label>
                     </Col>
                     <Col md="10">
-                        <input placeholder="Color" type="text" onChange={this.handleChange} />
+                        <input placeholder="Adjective" type="text" onChange={this.handleChange({inputTitle: 'adjectiveOne'})} />
                     </Col>
                </Row>
+                <Row>
+                   <Col md="12">
+                       <div className="inputDescription">Adjective</div>
+                   </Col>
+                </Row>
             </Col>
             <Col md="3" className="inputWrapper">
                <Row>
@@ -49,9 +79,14 @@ class MadlibForm extends Component {
                         <label className="greenlabel">1</label>
                     </Col>
                     <Col md="10">
-                        <input placeholder="Color" type="text" onChange={this.handleChange} />
+                        <input placeholder="Celebrity" type="text" onChange={this.handleChange({inputTitle: 'celebrityOne'})} />
                     </Col>
                </Row>
+                <Row>
+                   <Col md="12">
+                       <div className="inputDescription">Celebrity</div>
+                   </Col>
+                </Row>
             </Col>
           </Row>
         );
