f2ce2acded15aa318c029c9f8a7cd40db8b7f277
huntersfuntime <hunterorges@gmail.com>
2018-03-20 18:49:08 -0600

    Added content component with styles

--- src/components/madlib_form.js
+++ src/components/madlib_form.js
@@ -5,6 +5,7 @@ import {
     Card
 } from 'reactstrap';
 import _ from 'lodash';
+import MadlibContent from './madlib_content';
 
 function MadlibInput(props) {
     return (
@@ -59,6 +60,27 @@ class MadlibForm extends Component {
         event.preventDefault();
     }.bind(this);
 
+    handleClick = function () {
+           this.setState({
+            completedForm: false,
+            color: '',
+            pluralNoun: '',
+            adjectiveOne: '',
+            celebrityOne: '',
+            adjectiveTwo: '',
+            nounOne: '',
+            numberOne: '',
+            numberTwo: '',
+        })
+    }.bind(this)
+
+    renderButton = function() {
+        if(this.state.completedForm) {
+        return <a className="clear-button" onClick={this.handleClick}>Clear Mad Lib</a>
+        }
+        return <input type="submit" className="generate-button" value="Generate Mad Lib"/>
+    }
+
     render() { 
 
         this.inputData = [
@@ -88,11 +110,11 @@ class MadlibForm extends Component {
                     </Row>
                     <Row>
                         <Col md="12" className="button-wrapper">
-                            <input type="submit" className="generate-button" value="Generate Mad Lib"/>
+                           {this.renderButton()}
                         </Col>
                     </Row>
                 </form>
-              
+              <MadlibContent data={this.state}/>
             </Card>
           </div>
         );
